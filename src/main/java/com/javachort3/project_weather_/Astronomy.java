1b2563419889c98f7d4982011d06c3587bab047c
Jermaine34343 <Jermaine34343@gmail.com>
2018-05-02 13:53:52 -0400

    added

--- src/main/java/com/javachort3/project_weather_/Astronomy.java
+++ src/main/java/com/javachort3/project_weather_/Astronomy.java
@@ -1,25 +1,43 @@
-package com.javachort3.project_weather_;
+package com.jermaine.weatherastronomyWind;
 
 public class Astronomy {
     private String chill;
     private String direction;
     private String speed;
 
-    public Astronomy(String chill, String direction, String speed) {
-        this.chill = chill;
-        this.direction = direction;
-        this.speed = speed;
+    public Astronomy() {
     }
 
     public String getChill() {
         return chill;
     }
 
+    public void setChill(String chill) {
+        this.chill = chill;
+    }
+
     public String getDirection() {
         return direction;
     }
 
+    public void setDirection(String direction) {
+        this.direction = direction;
+    }
+
     public String getSpeed() {
         return speed;
+    }
+
+    public void setSpeed(String speed) {
+        this.speed = speed;
+    }
+
+    @Override
+    public String toString() {
+        return "Astronomy{" +
+                "chill='" + chill + '\'' +
+                ", direction='" + direction + '\'' +
+                ", speed='" + speed + '\'' +
+                '}';
     }
 }
--- src/main/java/com/javachort3/project_weather_/Wind.java
+++ src/main/java/com/javachort3/project_weather_/Wind.java
@@ -1,26 +1,43 @@
-package com.javachort3.project_weather_;
+package com.jermaine.weatherastronomyWind;
 
-public class Wind {
+public class wind {
     private String Chill;
     private String direction;
     private String speed;
 
-    public Wind(String chill, String direction, String speed) {
-        Chill = chill;
-        this.direction = direction;
-        this.speed = speed;
-    }
+    public wind() {
+        }
 
     public String getChill() {
         return Chill;
+    }
+
+    public void setChill(String chill) {
+        Chill = chill;
     }
 
     public String getDirection() {
         return direction;
     }
 
+    public void setDirection(String direction) {
+        this.direction = direction;
+    }
+
     public String getSpeed() {
         return speed;
     }
 
+    public void setSpeed(String speed) {
+        this.speed = speed;
+    }
+
+    @Override
+    public String toString() {
+        return "wind{" +
+                "Chill='" + Chill + '\'' +
+                ", direction='" + direction + '\'' +
+                ", speed='" + speed + '\'' +
+                '}';
+    }
 }
