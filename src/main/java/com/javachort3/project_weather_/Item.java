e07e8ecec4c2722df28be3bc9d17aec7aef5d555
Student <aermias@live.com>
2018-05-02 11:42:47 -0400

    added query and item classes

---
+++ src/main/java/com/javachort3/project_weather_/Item.java
@@ -0,0 +1,42 @@
+package aermias.Weather_API_Project;
+
+public class Item {
+    private String title;
+    private String lat;
+    private String long_;
+    private String link;
+    private String pubDate;
+    private Condition condition;
+    private Forecast[] forecasts;
+    private String description;
+    private GUID guid;
+
+    public Item() {}
+
+    public String getTitle() { return title; }
+    public void setTitle(String title) { this.title = title; }
+
+    public String getLat() { return lat; }
+    public void setLat(String lat) { this.lat = lat; }
+
+    public String getLong_() { return long_; } 
+    public void setLong_(String long_) { this.long_ = long_; }
+
+    public String getLink() { return link; }
+    public void setLink(String link) { this.link = link; }
+
+    public String getPubDate() { return pubDate; }
+    public void setPubDate(String pubDate) { this.pubDate = pubDate; }
+
+    public Condition getCondition() { return condition; }
+    public void setCondition(Condition condition) { this.condition = condition; }
+
+    public Forecast[] getForecasts() { return forecasts; }
+    public void setForecasts(Forecast[] forecasts) { this.forecasts = forecasts; }
+
+    public String getDescription() { return description; }
+    public void setDescription(String description) { this.description = description; }
+
+    public GUID getGuid() { return guid; }
+    public void setGuid(GUID guid) { this.guid = guid; }
+}
