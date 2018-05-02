27b1b601deb621005d9b9b6c889952658eab96c0
Student <drose.jr0427@gmail.com>
2018-05-02 13:32:47 -0400

    Adding getters and setters

--- src/main/java/com/javachort3/project_weather_/Forecast.java
+++ src/main/java/com/javachort3/project_weather_/Forecast.java
@@ -9,8 +9,54 @@ public class Forecast {
     private String low;
     private String text;
 
-    public Forecast(){
+    public Forecast(){}
 
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getDay() {
+        return day;
+    }
+
+    public void setDay(String day) {
+        this.day = day;
+    }
+
+    public String getHigh() {
+        return high;
+    }
+
+    public void setHigh(String high) {
+        this.high = high;
+    }
+
+    public String getLow() {
+        return low;
+    }
+
+    public void setLow(String low) {
+        this.low = low;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
     }
 
     @Override
--- src/main/java/com/javachort3/project_weather_/Location.java
+++ src/main/java/com/javachort3/project_weather_/Location.java
@@ -5,8 +5,30 @@ public class Location {
     private String country;
     private String region;
 
-    public Location(){
+    public Location(){}
 
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
     }
 
     @Override
