e0e7e14be2169db73e5d9d7950a8d5eff98c7ae4
senorcl0ud <claudemcalpin@gmail.com>
2018-05-02 15:47:52 -0400

    Added restTemplate to TaskShceduler class. Waiting on logger.

--- src/main/java/com/javachort3/project_weather_/TaskShcheduler.java
+++ src/main/java/com/javachort3/project_weather_/TaskShcheduler.java
@@ -1,19 +1,27 @@
 package com.javachort3.project_weather_;
 
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Arrays;
 
+@SpringBootApplication
 public class TaskShcheduler implements CommandLineRunner {
 
 
     @Bean
-    public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
+    public RestTemplate restTemplate(RestTemplateBuilder builder){
+        return builder.build();
+    }
+
+    @Bean
+    public CommandLineRunner run(RestTemplate restTxemplate) throws Exception {
         return args -> {
             logger.info("Application started with command-line arguments: {} . " +
-                    "\n To kill this application, press Ctrl + C.", Arrays.toString(args);
+                    "\n To kill this application, press Ctrl + C.", Arrays.toString(args));
         };
     }
 }
