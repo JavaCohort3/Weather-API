1f1452e96c4aba42ad66d46d2faa2bff29be9468
senorcl0ud <claudemcalpin@gmail.com>
2018-05-02 17:15:30 -0400

    refactored classname.

--- src/main/java/com/javachort3/project_weather_/TaskScheduler.java
+++ src/main/java/com/javachort3/project_weather_/TaskScheduler.java
@@ -19,7 +19,7 @@ public class TaskScheduler {
         }
 
         @Bean
-        public CommandLineRunner run(RestTemplate restTxemplate) throws Exception {
+        public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
             return args -> {
                 logger.info("Application started with command-line arguments: {} ." +
                         "\n To kill this application, press Ctrl + C. ", Arrays.toString(args));
