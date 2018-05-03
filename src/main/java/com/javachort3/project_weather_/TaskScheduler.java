3f726a27f1d2509fc1a83c328a238647fc07f96f
imowbray97 <isaiah.mowbray97@gmail.com>
2018-05-02 20:41:34 -0400

    #111 fix all problems with conflict

--- src/main/java/com/javachort3/project_weather_/TaskScheduler.java
+++ src/main/java/com/javachort3/project_weather_/TaskScheduler.java
@@ -1,17 +1,20 @@
 package com.javachort3.project_weather_;
 
 
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 import java.util.Arrays;
+import java.util.logging.Logger;
 
 
 @Component
 public class TaskScheduler {
 
+    private static final Logger log = (Logger) LoggerFactory.getLogger(Application.class);
 
         @Bean
         public RestTemplate restTemplate(RestTemplateBuilder builder){
@@ -21,7 +24,7 @@ public class TaskScheduler {
         @Bean
         public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
             return args -> {
-                logger.info("Application started with command-line arguments: {} . " +
+                log.info("Application started with command-line arguments: {} . " +
                         "\n To kill this application, press Ctrl + C. ", Arrays.toString(args));
             };
         }
