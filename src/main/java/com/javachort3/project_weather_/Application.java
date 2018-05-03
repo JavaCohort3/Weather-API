700dd9a979d31e11f051056b03122604575315b2
imowbray97 <isaiah.mowbray97@gmail.com>
2018-05-02 20:17:50 -0400

    #111 added Enable Task

--- src/main/java/com/javachort3/project_weather_/Application.java
+++ src/main/java/com/javachort3/project_weather_/Application.java
@@ -7,8 +7,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.client.RestTemplate;
-
+@EnableScheduling
 @SpringBootApplication
 public class Application {
 	private static final Logger log = LoggerFactory.getLogger(Application.class);
