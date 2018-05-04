eff75a52d39814dd37101184d1460682f3842e15
NightShadow3000 <RoyalFotoK@gmail.com>
2018-05-04 13:55:51 -0400

    Fixed the 400 error bug

--- src/main/java/com/javachort3/project_weather_/Application.java
+++ src/main/java/com/javachort3/project_weather_/Application.java
@@ -27,12 +27,13 @@ public class Application {
 		return builder.build();
 	}
 
-	@Bean @Scheduled(fixedRate = 30000)
+	@Scheduled(fixedRate = 30000)
+	@Bean
 	public static CommandLineRunner consumeAPI() throws Exception {
 		RestTemplate restTemplate = restTemplate(builder);
 		return args -> {
 			Query query = restTemplate.getForObject(
-					"https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22wilmington%2C%20de%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys", Query.class);
+					"https://query.yahooapis.com/v1/public/yql?q=select+*+from+weather.forecast+where+woeid+in+(select+woeid+from+geo.places(1)+where+text=\"wilmington, +de\")&format=json", Query.class);
 			log.info(query.toString());
 		};
 	}
--- src/main/java/com/javachort3/project_weather_/TaskScheduler.java
+++ src/main/java/com/javachort3/project_weather_/TaskScheduler.java
@@ -1,40 +1,40 @@
-package com.javachort3.project_weather_;
-
-
-import org.slf4j.LoggerFactory;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Component;
-import org.springframework.web.client.RestTemplate;
-import java.util.Arrays;
-import org.slf4j.Logger;
-
-
-@Component
-public class TaskScheduler {
-
-    private static final Logger log = LoggerFactory.getLogger(Application.class);
-
-    @Bean
-    public RestTemplate restTemplate(RestTemplateBuilder builder){
-        return builder.build();
-    }
-
-    @Bean
-    public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
-        return args -> {
-            log.info("Application started with command-line arguments: {} . " +
-                "\n To kill this application, press Ctrl + C. " + Arrays.toString(args));
-            };
-    }
-
-    @Scheduled(fixedRate = 30000)
-    public void reportWeather() {
-        log.info("The weather is now: " + "\n");
-        //consumeAPI();
-    }
-}
-
-
+//package com.javachort3.project_weather_;
+//
+//
+//import org.slf4j.LoggerFactory;
+//import org.springframework.boot.CommandLineRunner;
+//import org.springframework.boot.web.client.RestTemplateBuilder;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.scheduling.annotation.Scheduled;
+//import org.springframework.stereotype.Component;
+//import org.springframework.web.client.RestTemplate;
+//import java.util.Arrays;
+//import org.slf4j.Logger;
+//
+//
+//@Component
+//public class TaskScheduler {
+//
+//    private static final Logger log = LoggerFactory.getLogger(Application.class);
+//
+//    @Bean
+//    public RestTemplate restTemplate(RestTemplateBuilder builder){
+//        return builder.build();
+//    }
+//
+//    @Bean
+//    public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
+//        return args -> {
+//            log.info("Application started with command-line arguments: {} . " +
+//                "\n To kill this application, press Ctrl + C. " + Arrays.toString(args));
+//            };
+//    }
+//
+//    @Scheduled(fixedRate = 30000)
+//    public void reportWeather() {
+//        log.info("The weather is now: " + "\n");
+//        //consumeAPI();
+//    }
+//}
+//
+//
